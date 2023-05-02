f2962b0b3f706284730f1e0f0dd4cd2e21f93ef9
Oleksandr <oleksandryurievich@gmail.com>
2023-05-02 22:07:30 +0300

    feat:create second task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,1 +1,25 @@
-console.log("2");
+const fs = require("fs");
+const path = require("path");
+const process = require("node:process");
+
+const { stdin, stdout } = process;
+fs.writeFile(path.join(__dirname, "text.txt"), "", (err) => {
+  if (err) throw err;
+  console.log("Write your text:");
+});
+stdin.on("data", (data) => {
+  if (data.toString().slice(0, 4) == "exit") {
+    process.exit();
+  }
+  fs.appendFile(path.join(__dirname, "text.txt"), data, (err) => {
+    if (err) throw err;
+  });
+  process.on("exit", () => {
+    console.log(`Good bye!`);
+  });
+});
+process.on("SIGINT", () => {
+  console.log(`Good bye!`);
+
+  process.exit(0);
+});
