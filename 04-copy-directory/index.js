07f65ce028f21379234e00623891596055c53ad3
Oleksandr <oleksandryurievich@gmail.com>
2023-05-03 19:40:27 +0300

    feat: solve 4-th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+const fsPromises = fs.promises;
 
+fs.stat(path.join(__dirname, `files-copy`), function (err) {
+  if (!err) {
+  } else if (err.code === "ENOENT") {
+    fsPromises.mkdir(path.join(__dirname, `files-copy`), true);
+  }
+});
+fs.readdir(path.join(__dirname, "files"), (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      fs.copyFile(
+        path.join(__dirname, `files\\${file}`),
+        path.join(__dirname, `files-copy\\${file}`),
+        (err) => {
+          if (err) {
+            console.log("Error Found:", err);
+          }
+        }
+      );
+    });
+  }
+});
