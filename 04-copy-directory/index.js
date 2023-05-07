2458967aaf11cafc134d052511102274ff60ac8f
Oleksandr <oleksandryurievich@gmail.com>
2023-05-07 22:38:08 +0300

    fix: correct  4-th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,23 +4,42 @@ const fsPromises = fs.promises;
 
 fs.stat(path.join(__dirname, `files-copy`), function (err) {
   if (!err) {
+    fs.rm(path.join(__dirname, `files-copy`), { recursive: true }, (err) => {
+      fsPromises.mkdir(path.join(__dirname, `files-copy`), true);
+      fs.readdir(path.join(__dirname, "files"), (err, files) => {
+        if (err) console.log(err);
+        else {
+          files.forEach((file) => {
+            fs.copyFile(
+              path.join(__dirname, `files\\${file}`),
+              path.join(__dirname, `files-copy\\${file}`),
+              (err) => {
+                if (err) {
+                  console.log("Error Found:", err);
+                }
+              }
+            );
+          });
+        }
+      });
+    });
   } else if (err.code === "ENOENT") {
     fsPromises.mkdir(path.join(__dirname, `files-copy`), true);
-  }
-});
-fs.readdir(path.join(__dirname, "files"), (err, files) => {
-  if (err) console.log(err);
-  else {
-    files.forEach((file) => {
-      fs.copyFile(
-        path.join(__dirname, `files\\${file}`),
-        path.join(__dirname, `files-copy\\${file}`),
-        (err) => {
-          if (err) {
-            console.log("Error Found:", err);
-          }
-        }
-      );
+    fs.readdir(path.join(__dirname, "files"), (err, files) => {
+      if (err) console.log(err);
+      else {
+        files.forEach((file) => {
+          fs.copyFile(
+            path.join(__dirname, `files\\${file}`),
+            path.join(__dirname, `files-copy\\${file}`),
+            (err) => {
+              if (err) {
+                console.log("Error Found:", err);
+              }
+            }
+          );
+        });
+      }
     });
   }
 });
